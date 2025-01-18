307d58d0b962b4d709b40a62377092420e4a209a
Bernabé Pando Fernández <bernabepando@gmail.com>
2025-01-18 19:21:23 +0100

    portatil

--- controllers/PonentesController.php
+++ controllers/PonentesController.php
@@ -10,9 +10,10 @@ class PonentesController
 {
     public static function index(Router $router)
     {
-
+        $ponentes = Ponente::all();
         $router->render('/admin/ponentes/index', [
-            'titulo' => 'Ponentes / Conferencistas'
+            'titulo' => 'Ponentes / Conferencistas',
+            'ponentes' => $ponentes
         ]);
     }
 
