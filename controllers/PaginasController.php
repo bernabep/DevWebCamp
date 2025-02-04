63a0e3ad0412158a7f61d826f41436c2dacd9da7
Bernabep <bernabepando@gmail.com>
2025-02-04 20:49:20 +0100

    sobremesa

--- controllers/PaginasController.php
+++ controllers/PaginasController.php
@@ -98,4 +98,10 @@ class PaginasController
             'eventos' => $eventos_formateados
         ]);
     }
+
+    public static function error(Router $router){
+        $router->render('paginas/error', [
+            'titulo' => 'Pagina no encontrada'
+        ]);
+    }
 }
