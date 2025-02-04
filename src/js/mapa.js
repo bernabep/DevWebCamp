fa31119f3491ad0f023cd65b09f1b77784ba1bfe
Bernabep <bernabepando@gmail.com>
2025-02-04 19:58:32 +0100

    sobremesa

--- src/js/mapa.js
+++ src/js/mapa.js
@@ -14,7 +14,7 @@ if (document.querySelector("#mapa")) {
     .addTo(map)
     .bindPopup(`
         <h2 class="mapa__heading">DevWebCamp</h2>
-        <p class="mapa__texto">Centro de Convernciones de Sevilla</p>
+        <p class="mapa__texto">Centro de Convenciones de Sevilla</p>
         `)
     .openPopup();
 }
